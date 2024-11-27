35067a08d9d521b6c40ca1714ae78b1612623cd8
Daniel Brooks <danielbrooks20@gmail.com>
2024-11-27 09:47:48 -0800

    November 24 updates - MLP approach
    MLP model before cleanup
    
    Add gitignore
    
    Update license
    
    Remove large files
    
    Remove files, sketch out python package
    
    Sketch of python package.
    
    Remove DS_Store
    
    Update gitignore

---
+++ statisticaldrafting/__init__.py
@@ -0,0 +1,9 @@
+# TODO: refactor notebooks into importable python package. 
+
+# Structure:
+# ├── statisticaldrafting/      # Main package directory
+# │   ├── __init__.py           # Package initialization
+# │   ├── training.py           # Training-related functions
+# │   ├── training_set.py       # Functions for creating training sets
+# │   ├── deployment.py         # Functions for deployment
+# │   ├── utils.py              # Optional: helper functions (if needed)
