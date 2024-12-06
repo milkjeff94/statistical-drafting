3eb742a47b587dd2b1e8e9444a9024327c402319
Daniel Brooks <danielbrooks20@gmail.com>
2024-12-05 20:47:05 -0800

    Minor tweaks to deckbuild assistant.

--- statisticaldrafting/draftassistant.py
+++ statisticaldrafting/draftassistant.py
@@ -11,7 +11,7 @@ class DraftModel:
     def __init__(self, set: str="FDN"): 
         # Get data from set. 
         self.set = set
-        self.pick_table = pd.read_csv(f"../data/cards/{self.set}.csv") # will be sorted. 
+        self.pick_table = pd.read_csv(f"../data/cards/{self.set}.csv") # will be sorted.
         self.cardnames = self.pick_table["name"].tolist()
 
         # Load model. 
@@ -24,21 +24,29 @@ class DraftModel:
         self.pick_table["synergy"] = [0.0] * len(self.pick_table)
         self.pick_table["rating"] = [0.0] * len(self.pick_table)
 
-    def get_card_ratings(self, collection: List[Union[str, int]]) -> pd.Series:
-        """ Get card ratings (0-150) for input collection."""
-        # Create collection vector. 
+    def get_collection_vector(self, collection: List[Union[str, int]]) -> torch.Tensor:
+        """Get a collection vector from a list of cardnames and card_ids"""
         collection_vector = torch.zeros([1, len(self.cardnames)])        
         for card in collection: 
             if type(card) is str:
                 # Validate cardname. 
                 if card not in self.cardnames:
-                    raise Exception(f"{card} not in set. Please correct cardname.")
+                    if card not in ["Plains", "Island", "Forest", "Swamp", "Mountain"]:
+                        print(f"{card} not in draftable set.")
+                    continue
                 card_index = self.cardnames.index(card)
             else:
                 # card_id is input. 
                 card_index = card
             collection_vector[0, card_index] += 1
-            
+        return collection_vector
+
+
+    def get_card_ratings(self, collection: List[Union[str, int]]) -> pd.Series:
+        """ Get card ratings (0-150) for input collection."""
+        # Create collection vector. 
+        collection_vector = self.get_collection_vector(collection)
+        
         # Get raw card scores. 
         self.network.eval()
         with torch.no_grad():
@@ -65,3 +73,60 @@ class DraftModel:
         # Return sorted values. 
         self.pick_table = self.pick_table.sort_values(by="rating", ascending=False)
         return self.pick_table
+
+    def get_deck_recommendation(self, pool_cardnames: List[Union[str, int]], starting_colors: str="") -> pd.DataFrame:
+        """
+        Constructs a deck from the cards in a pool. 
+
+        Works by iteratively computing the "pick order" for cards in the deck. 
+
+        The starting deck can be specified by suggesting colors in "WUBRG".
+
+        WANRING: This is an experimental feature - expect to correct some picks in the deck. 
+        """
+        # Fixed parameters. 
+        DECK_SIZE = 25 # Cards in deck - TODO: account for nonbasic lands precisely.
+        NUM_ITERATION = 15
+        
+        # Initialize pool. 
+        valid_cardnames = [cn for cn in pool_cardnames if cn in self.cardnames]
+        df_pool = pd.DataFrame(valid_cardnames, columns=["name"])
+        df_pool = pd.merge(df_pool, self.pick_table[["name", "color_identity"]], on='name', how='left')
+
+        # Validate starting colors.
+        if not set(starting_colors.upper()).issubset(set("WUBGR")):
+            raise Exception("Starting colors should be a subset of WUBGR. Please adjust starting colors.")
+
+        # Allow user to suggest colors for initial deck. 
+        if len(starting_colors) > 0:
+            colors_to_include = ["Colorless"] # TODO: account for multicolored correctly in the first iteration. 
+            for color in starting_colors.upper():
+                if color in "WUBGR":
+                    colors_to_include.append(color)
+            cur_deck = df_pool[df_pool["color_identity"].isin(colors_to_include)]["name"].tolist()
+        else:
+            cur_deck = []
+
+        # Iteratively refine deck. 
+        for i in range(NUM_ITERATION):
+            deck_pickorder = self.get_pick_order(cur_deck)
+            deck_cards_ranked = pd.merge(df_pool[["name"]], deck_pickorder[["name", "color_identity", "rating"]], on='name', how='left')
+            deck_cards_ranked = deck_cards_ranked.sort_values(by="rating", ascending=False)
+            cur_deck = [name for name in deck_cards_ranked["name"].head(DECK_SIZE)] 
+
+        # Return final deck. 
+        return deck_cards_ranked
+    
+def parse_cardnames(card_str, set="FDN"):
+    """
+    Get cardnames from arena deck export. 
+    """
+    rows = card_str.split("\n")
+    pool_cardnames = []
+    for row in rows:
+        if f"({set})" in row:
+            number = int(row.split(" ")[0])
+            name = row.split("(")[0][2:-1]
+            for _ in range(number):
+                pool_cardnames.append(name)
+    return pool_cardnames
