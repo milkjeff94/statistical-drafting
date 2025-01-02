9faf1264e6cb01b47117ac155344488e2cd4444c
Daniel Brooks <danielbrooks20@gmail.com>
2025-01-02 15:48:56 -0800

    Debug statement on card scores for colab

--- statisticaldrafting/draftassistant.py
+++ statisticaldrafting/draftassistant.py
@@ -64,12 +64,13 @@ class DraftModel:
             )
         card_scores = card_scores_torch.reshape(-1).tolist() # List of scores. 
 
+        print(f"card_scores {type(card_scores)} {card_scores}")
         # Get card scores.
-        min_score = float(min(card_scores))
-        max_score = float(max(card_scores))
+        # min_score = float(min(card_scores))
+        # max_score = float(max(card_scores))
 
         # Get card scores.
-        card_score_series = pd.Series(card_scores)  # index is card id
+        card_score_series = pd.Series(card_scores, name="scores")  # index is card id
 
         # New rating logic - average card in set is 50.0. 
         mean = card_score_series.mean()
